68d0320ef8d5104e334784c40f5f2f1b2317c92d
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2022-01-12 18:50:11 +1100

    fixed flex answer layout

--- src/components/card.js
+++ src/components/card.js
@@ -11,8 +11,8 @@ function Card({ questions }) {
                 ))}
             </div>
             <div className="flex justify-center">
-                <div className=" flex  justify-center">
-                    <div className="  flex flex-row w-3/4">
+                <div className=" flex  justify-center w-1/2">
+                    <div className="   ">
                         {questions[0].answers.map((element) => (
                             <button className="font-mono rounded-lg border-solid border-2 p-4 m-4 w-72 text-center shadow-lg">
                                 {element}
